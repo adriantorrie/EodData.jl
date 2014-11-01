fc1d69a66c7965584a3de58f255b75d9476f1c79
Adrian Torrie <adriantorrie@gmail.com>
2014-11-01 17:45:46 +1100

    Adding test files

--- test/eod_utils_external.jl
+++ test/eod_utils_external.jl
@@ -1,17 +1,27 @@
-using EodData
-using Dates
+#=
+	Unit tests for EodData external utility functions
+=#
 using Base.Test
+using EodData
+if VERSION < v"0.4-"
+	using Dates
+else
+	using Base.Dates
+end
 
 dt = DateTime("20140101", "yyyymmdd")
 d = Date("20140101", "yyyymmdd")
 
-# set_date_string()
+# set_date_string() with a ::DateTime
 @test set_date_string(dt) == "20140101"
 @test set_date_string(dt) != 20140101
 @test set_date_string(dt) != dt
 @test set_date_string(dt) != d
+@test typeof(set_date_string(dt)) == typeof("20140101")
 
+# set_date_string() with a ::Date
 @test set_date_string(d) == "20140101"
 @test set_date_string(d) != 20140101
 @test set_date_string(d) != dt
 @test set_date_string(d) != d
+@test typeof(set_date_string(d)) == typeof("20140101")
--- test/eod_utils_internal.jl
+++ test/eod_utils_internal.jl
@@ -1,12 +1,21 @@
+#=
+	Unit tests for EodData internal utility functions
+=#
+using Base.Test
+if VERSION < v"0.4-"
+	using Dates
+else
+	using Base.Dates
+end
 using EodData
 using HTTPClient.HTTPC
 using LibExpat
-using Base.Test
 
 include("../src/eod_utils_internal.jl")
 
 response = HTTPC.post("http://requestb.in/api/v1/bins", "")
 validate_http_response(response)
+
 
 # set_date_string()
 @test response.http_code == 200 ? validate_http_response(response) : true
